11b8ca91d901d1ebb832852b8d3fde0241132919
Max Horn <max.horn@bsse.ethz.ch>
2021-01-19 10:28:21 +0100

    Setup skeleton for a torch CppExtension

---
+++ build.py
@@ -0,0 +1,24 @@
+from setuptools import setup, Extension
+from torch.utils import cpp_extension
+
+torch_library_paths = cpp_extension.library_paths(cuda=False)
+
+
+def build(setup_kwargs):
+    """
+    This function is mandatory in order to build the extensions.
+    """
+    setup_kwargs.update({
+        'ext_modules': [
+            cpp_extension.CppExtension(
+                'torch_persistent_homology.unionfind',
+                ['torch_persistent_homology/unionfind.cpp'],
+                extra_link_args=[
+                    '-Wl,-rpath,' + library_path
+                    for library_path in torch_library_paths]
+            )
+        ],
+        'cmdclass': {
+            'build_ext': cpp_extension.BuildExtension
+        }
+    })
---
+++ tests/test_unionfind_extension.py
@@ -0,0 +1,2 @@
+def test_loading():
+    import torch_persistent_homology.unionfind
