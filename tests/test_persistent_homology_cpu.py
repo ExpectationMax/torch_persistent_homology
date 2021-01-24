f23c7617e459287c117c5dab340c970be137a9ec
Max Horn <max.horn@bsse.ethz.ch>
2021-01-24 23:03:24 +0100

    Preliminary implementation of persistent homology computation in torch C++

--- tests/test_persistent_homology_cpu.py
+++ tests/test_persistent_homology_cpu.py
@@ -1,13 +1,109 @@
 import torch
 from torch_persistent_homology import persistent_homology_cpu
+from pyper.utilities import UnionFind
 
 
-def test_set_to_one():
-    z = torch.zeros(50)
-    persistent_homology_cpu.set_to_one(z)
-    assert (z == 1.).all()
+def persistence_routine(filtered_v_, edge_indices, cycles=False):
+    """
+    Pytorch based routine to compute the persistence pairs
+    Based on pyper routine.
+    Inputs :
+        * filtration values of the vertices
+        * data object that stores the graph structure (could be just the edge_index actually)
+        * method is just a check for the algo
+        * cycles is a boolean to compute the 1D persistence or not. If true, returns also the 1D persistence.
+    """
+
+    # Quick check for the filtration values to be different.
+    # if torch.unique(filtered_v_).reshape(-1,1).shape[0] != filtered_v_.reshape(-1,1).shape[0]:
+    # if not unique, we add a small perturbation on all the values with std 0.01 x the initial std of the filtration values.
+    # std = torch.std(filtered_v_)
+    # filtered_v_ += 0.001*std*torch.randn(filtered_v_.shape)
+
+    # Compute the edge filtrations as the max between the value of the nodes.
+
+    filtered_e_, _ = torch.max(torch.stack(
+        (filtered_v_[edge_indices[0]], filtered_v_[edge_indices[1]])), axis=0)
+
+    # Only the edges need to be sorted, since they determine the
+    # filtration ordering. For the vertices, we will look up the
+    # values directly in the tensor.
+    filtered_e, e_indices = torch.sort(filtered_e_)
+
+    n_vertices = len(filtered_v_)
+
+    uf = UnionFind(n_vertices)
+
+    persistence = torch.zeros(
+        (n_vertices, 2),
+        device=filtered_v_.device
+    )
+    if cycles:
+        persistence1 = torch.zeros(
+            (len(filtered_e), 2), device=filtered_v_.device)
+
+    edge_indices_cycles = []
+
+    unpaired_value = filtered_e[-1]
+
+    persistence[:, 0] = filtered_v_
+
+    for edge_index, edge_weight in zip(e_indices, filtered_e):
+
+        # nodes connected to this edge
+        nodes = edge_indices[:, edge_index]
+
+        younger = uf.find(nodes[0])
+        older = uf.find(nodes[1])
+
+        if younger == older:
+            if cycles:
+                # edge_indices_cycles.append(edge_index)
+                persistence1[edge_index, 0] = filtered_e_[edge_index]
+                persistence1[edge_index, 1] = unpaired_value
+            continue
+        else:
+            # Use vertex weight lookup to determine which vertex comes
+            # first. This works because our filtrations are based on
+            # values at the vertices themselves.
+            if filtered_v_[younger] < filtered_v_[older]:
+                younger, older = older, younger
+                nodes = torch.flip(nodes, [0])
+
+        # persistence[younger, 0] = filtered_v_[younger]
+        persistence[younger, 1] = edge_weight
+
+        uf.merge(nodes[0], nodes[1])
+
+    # TODO : this currently assumes a single unpaired value for the whole batch. THis can be discussed.
+    for root in uf.roots():
+        persistence[root, 0] = filtered_v_[root]
+        persistence[root, 1] = unpaired_value
+
+    # if cycles:
+    # persistence1 = torch.zeros((len(filtered_e),2), device = filtered_v_.device)
+    # for edge_index in edge_indices_cycles:
+    #    persistence1[edge_index,0] = filtered_e_[edge_index]
+    #    persistence1[edge_index,1] = unpaired_value
+
+    # cycle_time = time.time()
+
+    if cycles:
+        return persistence, persistence1
+    else:
+        return persistence
 
 
-def test_ones_tensor():
-    z = persistent_homology_cpu.ones_tensor()
-    assert (z == 1.).all()
+def test_persistence_computation():
+    edge_index = torch.Tensor([[0, 0, 2, 3, 4], [2, 3, 3, 4, 1]]).long()
+    filtered_v = torch.Tensor([1., 1., 2., 3., 4.])
+    filtered_e, _ = torch.max(torch.stack(
+        (filtered_v[edge_index[0]], filtered_v[edge_index[1]])),
+        axis=0)
+    impl1 = persistence_routine(filtered_v, edge_index)
+    impl2 = persistent_homology_cpu.persistence_routine(
+        filtered_v, filtered_e, edge_index)
+
+    print(impl1)
+    print(impl2)
+    assert (impl1 == impl2).all()
--- torch_persistent_homology/perisistent_homology_cpu.cpp
+++ torch_persistent_homology/perisistent_homology_cpu.cpp
@@ -1,6 +1,10 @@
+#include "ATen/core/function_schema.h"
 #include "unionfind.hh"
 #include <ATen/Parallel.h>
+#include <iostream>
 #include <torch/extension.h>
+
+using namespace torch::indexing;
 
 template <typename dtype> void set_to_one(at::TensorAccessor<dtype, 1> z) {
   at::parallel_for(0, z.size(0), 0, [&](int64_t begin, int64_t end) {
@@ -46,9 +50,71 @@ void uf_merge(torch::Tensor parents, int u, int v) {
                              }));
 }
 
+torch::Tensor compute_persistence_homology(torch::Tensor filtered_v,
+                                           torch::Tensor filtered_e,
+                                           torch::Tensor edge_index) {
+  auto n_vertices = filtered_v.size(0);
+  auto n_edges = filtered_e.size(0);
+  auto parents = torch::arange(0, n_vertices);
+  auto parents_data = parents.accessor<int64_t, 1>();
+  auto persistence =
+      torch::zeros(at::IntArrayRef({n_vertices, 2}), filtered_v.options());
+  auto persistence1 = torch::zeros({n_edges, 2}, filtered_e.options());
+
+  // Looks like the more eligant alternative is C++17 thus we will have to live
+  // with this.
+  torch::Tensor filtered_e_sorted, sorted_indices;
+  auto sorted_out = filtered_e.sort();
+  filtered_e_sorted = std::get<0>(sorted_out);
+  sorted_indices = std::get<1>(sorted_out);
+
+  persistence.index_put_({Ellipsis, 0}, filtered_v);
+  std::cout << "persistence:" << persistence << std::endl;
+  auto unpaired_value = filtered_e.index({-1});
+
+  for (auto i = 0; i < n_edges; i++) {
+    auto cur_edge_index = sorted_indices[i].item<int64_t>();
+    auto cur_edge_weight = filtered_e_sorted[i].item<float>();
+    auto nodes = edge_index.index({Ellipsis, cur_edge_index});
+    std::cout << "nodes:" << nodes << std::endl;
+
+    auto node1 = nodes[0].item<int64_t>();
+    auto node2 = nodes[1].item<int64_t>();
+
+    auto younger = UnionFind<int64_t>::find(parents_data, node1);
+    auto older = UnionFind<int64_t>::find(parents_data, node2);
+    if (younger == older) {
+      persistence1.index_put_({edge_index, 0}, cur_edge_weight);
+      persistence1.index_put_({edge_index, 1}, unpaired_value);
+      continue;
+    } else {
+      if (filtered_v[younger].item<float>() < filtered_v[older].item<float>()) {
+        // Filp older and younger, node1 and node 2
+        auto tmp = younger;
+        younger = older;
+        older = tmp;
+        tmp = node1;
+        node1 = node2;
+        node2 = tmp;
+      }
+    }
+
+    persistence.index_put_({younger, 1}, cur_edge_weight);
+    UnionFind<int64_t>::merge(parents_data, node1, node2);
+  }
+  // Collect roots
+  auto is_root = parents == torch::arange(0, n_vertices, parents.options());
+  auto root_values = filtered_v.index({is_root});
+  persistence.index_put_({is_root, 0}, root_values);
+  persistence.index_put_({is_root, 1}, unpaired_value);
+  return persistence;
+}
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("set_to_one", &set_to_one_tensor, "Test inplace parallel set to one");
   m.def("ones_tensor", &ones_tensor, "Test parallel set to one");
   m.def("uf_find", &uf_find, "UnionFind find operation");
   m.def("uf_merge", &uf_merge, "UnionFind merge operation");
+  m.def("persistence_routine", &compute_persistence_homology,
+        "Persistence routine");
 }
