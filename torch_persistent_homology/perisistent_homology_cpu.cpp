3cc1992b9adeccc365a66823eb8b51dafa5ceb96
Max Horn <max.horn@bsse.ethz.ch>
2021-01-25 15:43:41 +0100

    Final fixes for computation of persistent homology

--- torch_persistent_homology/perisistent_homology_cpu.cpp
+++ torch_persistent_homology/perisistent_homology_cpu.cpp
@@ -50,9 +50,9 @@ void uf_merge(torch::Tensor parents, int u, int v) {
                              }));
 }
 
-torch::Tensor compute_persistence_homology(torch::Tensor filtered_v,
-                                           torch::Tensor filtered_e,
-                                           torch::Tensor edge_index) {
+std::tuple<torch::Tensor, torch::Tensor>
+compute_persistence_homology(torch::Tensor filtered_v, torch::Tensor filtered_e,
+                             torch::Tensor edge_index) {
   auto n_vertices = filtered_v.size(0);
   auto n_edges = filtered_e.size(0);
   auto parents = torch::arange(0, n_vertices);
@@ -62,14 +62,13 @@ torch::Tensor compute_persistence_homology(torch::Tensor filtered_v,
 
   // Looks like the more eligant alternative is C++17 thus we will have to live
   // with this.
-  torch::Tensor filtered_e_sorted, sorted_indices;
   auto sorted_out = filtered_e.sort();
-  filtered_e_sorted = std::get<0>(sorted_out);
-  sorted_indices = std::get<1>(sorted_out);
+  auto &&filtered_e_sorted = std::get<0>(sorted_out);
+  auto &&sorted_indices = std::get<1>(sorted_out);
 
   persistence.index_put_({Ellipsis, 0}, filtered_v);
   // std::cout << "persistence:" << persistence << std::endl;
-  auto unpaired_value = filtered_e.index({-1});
+  auto unpaired_value = filtered_e_sorted.index({-1});
 
   for (auto i = 0; i < n_edges; i++) {
     auto cur_edge_index = sorted_indices[i].item<int64_t>();
@@ -83,12 +82,12 @@ torch::Tensor compute_persistence_homology(torch::Tensor filtered_v,
     auto younger = UnionFind<int64_t>::find(parents_data, node1);
     auto older = UnionFind<int64_t>::find(parents_data, node2);
     if (younger == older) {
-      persistence1.index_put_({edge_index, 0}, cur_edge_weight);
-      persistence1.index_put_({edge_index, 1}, unpaired_value);
+      persistence1.index_put_({cur_edge_index, 0}, cur_edge_weight);
+      persistence1.index_put_({cur_edge_index, 1}, unpaired_value);
       continue;
     } else {
       if (filtered_v[younger].item<float>() < filtered_v[older].item<float>()) {
-        // Filp older and younger, node1 and node 2
+        // Flip older and younger, node1 and node 2
         auto tmp = younger;
         younger = older;
         older = tmp;
@@ -106,14 +105,14 @@ torch::Tensor compute_persistence_homology(torch::Tensor filtered_v,
   auto root_values = filtered_v.index({is_root});
   persistence.index_put_({is_root, 0}, root_values);
   persistence.index_put_({is_root, 1}, unpaired_value);
-  return persistence;
+  // persistence.index_put_({is_root, 1}, -1);
+  return std::make_tuple(std::move(persistence), std::move(persistence1));
 }
 
-torch::Tensor compute_persistence_homology_batched(torch::Tensor filtered_v,
-                                                   torch::Tensor filtered_e,
-                                                   torch::Tensor edge_index,
-                                                   torch::Tensor vertex_slices,
-                                                   torch::Tensor edge_slices) {
+std::tuple<torch::Tensor, torch::Tensor> compute_persistence_homology_batched(
+    torch::Tensor filtered_v, torch::Tensor filtered_e,
+    torch::Tensor edge_index, torch::Tensor vertex_slices,
+    torch::Tensor edge_slices) {
 
   auto batch_size = vertex_slices.size(0) - 1;
   auto n_nodes = filtered_v.size(0);
@@ -125,29 +124,26 @@ torch::Tensor compute_persistence_homology_batched(torch::Tensor filtered_v,
                      .repeat({n_filtrations, 1});
   auto persistence =
       torch::zeros({n_filtrations, n_nodes, 2}, filtered_v.options());
-  auto persistence1 = torch::zeros({n_edges, n_nodes, 2}, filtered_v.options());
+  auto persistence1 =
+      torch::zeros({n_filtrations, n_edges, 2}, filtered_v.options());
   auto vertex_slices_data = vertex_slices.accessor<int64_t, 1>();
   auto edge_slices_data = edge_slices.accessor<int64_t, 1>();
   for (auto i = 0; i < batch_size * n_filtrations; i++) {
     auto instance = i / n_filtrations;
     auto filtration = i % n_filtrations;
-    auto cur_vertices = filtered_v.index(
-        {Slice(vertex_slices_data[instance], vertex_slices_data[instance + 1]),
-         filtration});
-    auto cur_edges = filtered_e.index(
-        {Slice(edge_slices_data[instance], edge_slices_data[instance + 1]),
-         filtration});
+    auto vertex_slice =
+        Slice(vertex_slices_data[instance], vertex_slices_data[instance + 1]);
+    auto edge_slice =
+        Slice(edge_slices_data[instance], edge_slices_data[instance + 1]);
+    auto cur_vertices = filtered_v.index({vertex_slice, filtration});
+    auto cur_edges = filtered_e.index({edge_slice, filtration});
     auto vertex_offset = vertex_slices_data[instance];
     auto cur_edge_indices =
-        edge_index.index({Ellipsis, Slice(edge_slices_data[instance],
-                                          edge_slices_data[instance + 1])}) -
-        vertex_offset;
+        edge_index.index({Ellipsis, edge_slice}) - vertex_offset;
     auto cur_res =
         compute_persistence_homology(cur_vertices, cur_edges, cur_edge_indices);
-    persistence.index_put_(
-        {filtration,
-         Slice(vertex_slices_data[instance], vertex_slices_data[instance + 1])},
-        cur_res);
+    persistence.index_put_({filtration, vertex_slice}, std::get<0>(cur_res));
+    persistence1.index_put_({filtration, edge_slice}, std::get<1>(cur_res));
   }
   // Below code does not work due to usage of at:Tensors in threads. Need to
   // rewrite inner part of loop to not use at:Tensor but only raw memory access.
@@ -174,7 +170,7 @@ torch::Tensor compute_persistence_homology_batched(torch::Tensor filtered_v,
   //                               cur_edge_indices));
   //       }
   //     });
-  return persistence;
+  return std::make_tuple(persistence, persistence1);
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
